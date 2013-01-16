a4cc95f6f80f91514d40f9bc28a189ec97d344ed
Dmitry Orekhov <Dmitry.Orekhov@gmail.com>
2013-01-15 18:25:29 -0800

    Flow_mod improvement, stage 2

--- src/main/java/org/flowforwarding/of/controller/protocol/Protocol.java
+++ src/main/java/org/flowforwarding/of/controller/protocol/Protocol.java
@@ -52,6 +52,7 @@ public class Protocol {
 		ofpFlowModBaseSchema = protocol.getType("of12.ofp_flow_mod_base");
 		ofpTypeSchema = protocol.getType("of12.ofp_type");
 		ofpConfigFlagsSchema = protocol.getType("of12.ofp_config_flags");
+		ofpFlowModFlagsSchema = protocol.getType("ofp_flow_mod_flags");
 		ofpMatchTypeSchema = protocol.getType("of12.ofp_match_type");
 		
 		
@@ -155,6 +156,7 @@ public class Protocol {
 		
 		GenericRecord ofpHeaderRecord = new GenericData.Record(ofpHeaderSchema);
 		GenericRecord ofpSwitchConfigRecord = new GenericData.Record(ofpSwitchConfigSchema);
+		GenericRecord ofpConfigFlagsRecord = new GenericData.Record(ofpConfigFlagsSchema);
 		
 		byte[] ver = {3};    
 		GenericData.Fixed version = new GenericData.Fixed(ofpHeaderSchema, ver); 
@@ -172,8 +174,12 @@ public class Protocol {
       
 	   ofpSwitchConfigRecord.put("header", ofpHeaderRecord);
       
-      
-      ofpSwitchConfigRecord.put("flags", new EnumSymbol(ofpConfigFlagsSchema, "OFPC_FRAG_NORMAL"));
+	   byte[] fpad = {0};
+	   GenericData.Fixed flPad = new GenericData.Fixed(ofpConfigFlagsSchema, fpad);
+	   ofpConfigFlagsRecord.put("pad", flPad);
+	   ofpConfigFlagsRecord.put("flag", new EnumSymbol(ofpConfigFlagsSchema, "OFPC_FRAG_NORMAL"));
+	   	   
+	   ofpSwitchConfigRecord.put("flags", ofpConfigFlagsRecord);
 
       byte[] msl = {0,0};
       GenericData.Fixed miss_send_len = new GenericData.Fixed(ofpSwitchConfigSchema, msl);
@@ -198,6 +204,7 @@ public class Protocol {
 		GenericRecord ofpFlowModRecord = new GenericData.Record(ofpFlowModSchema);
 		GenericRecord ofpFlowModBaseRecord = new GenericData.Record(ofpFlowModBaseSchema);
 		GenericRecord ofpMatchRecord = new GenericData.Record(ofpMatchSchema);
+		GenericRecord ofpFlowModFlagsRecord = new GenericData.Record(ofpFlowModFlagsSchema);
 		
       byte[] ver = {3};    
       GenericData.Fixed version = new GenericData.Fixed(ofpHeaderSchema, ver); 
@@ -213,7 +220,7 @@ public class Protocol {
       GenericData.Fixed xid = new GenericData.Fixed(ofpHeaderSchema, xd);
       ofpHeaderRecord.put("xid", xid);
       
-		ofpFlowModRecord.put("header", ofpHeaderRecord);
+		ofpFlowModBaseRecord.put("header", ofpHeaderRecord);
 		
       byte[] ck = {0,0,0,0,0,0,0,0};
       GenericData.Fixed cookie = new GenericData.Fixed(ofpFlowModBaseSchema, ck);
@@ -221,62 +228,67 @@ public class Protocol {
 		
       byte[] cm = {0,0,0,0,0,0,0,0};
       GenericData.Fixed cookie_mask = new GenericData.Fixed(ofpFlowModBaseSchema, cm);
-      ofpFlowModRecord.put("cookie_mask", cookie_mask);
+      ofpFlowModBaseRecord.put("cookie_mask", cookie_mask);
       
       byte[] tid = {0};
       GenericData.Fixed table_id = new GenericData.Fixed(ofpFlowModBaseSchema, tid);
-      ofpFlowModRecord.put("table_id", table_id);
+      ofpFlowModBaseRecord.put("table_id", table_id);
       
-      ofpFlowModRecord.put("command", new EnumSymbol(ofpFlowModCommandSchema, "OFPFC_MODIFY"));
+      ofpFlowModBaseRecord.put("command", new EnumSymbol(ofpFlowModCommandSchema, "OFPFC_MODIFY"));
       
       byte[] itb = {0,0};
       GenericData.Fixed idle_timeout = new GenericData.Fixed(ofpFlowModBaseSchema, itb);
-      ofpFlowModRecord.put("idle_timeout", idle_timeout);
+      ofpFlowModBaseRecord.put("idle_timeout", idle_timeout);
       
       byte[] htb = {0,0};
       GenericData.Fixed hard_timeout = new GenericData.Fixed(ofpFlowModBaseSchema, htb);
-      ofpFlowModRecord.put("hard_timeout", hard_timeout);
+      ofpFlowModBaseRecord.put("hard_timeout", hard_timeout);
       
       byte[] prior = {0,0};
       GenericData.Fixed priority = new GenericData.Fixed(ofpFlowModBaseSchema, prior);
-      ofpFlowModRecord.put("priority", priority);
+      ofpFlowModBaseRecord.put("priority", priority);
       
       byte[] bid = {0,0,0,0};
       GenericData.Fixed buffer_id = new GenericData.Fixed(ofpFlowModBaseSchema, bid);
-      ofpFlowModRecord.put("buffer_id", buffer_id);
+      ofpFlowModBaseRecord.put("buffer_id", buffer_id);
       
       byte[] op = {0,0,0,0};
       GenericData.Fixed out_port = new GenericData.Fixed(ofpFlowModBaseSchema, op);
-      ofpFlowModRecord.put("out_port", out_port);        
+      ofpFlowModBaseRecord.put("out_port", out_port);        
       
       byte[] og = {0,0,0,0};
       GenericData.Fixed out_group = new GenericData.Fixed(ofpFlowModBaseSchema, og);
-      ofpFlowModRecord.put("out_group", out_group);
-              
-      ofpFlowModRecord.put("flags", new EnumSymbol(ofpFlowModFlagsSchema, "OFPFF_SEND_FLOW_REM"));
+      ofpFlowModBaseRecord.put("out_group", out_group);
+      
+      byte[] fpad = {0};
+      GenericData.Fixed flPad = new GenericData.Fixed(ofpFlowModFlagsSchema, fpad);
+      ofpFlowModFlagsRecord.put("pad", flPad);
+      ofpFlowModFlagsRecord.put("flag", new EnumSymbol(ofpFlowModFlagsSchema, "OFPFF_SEND_FLOW_REM"));
+            
+      ofpFlowModBaseRecord.put("flags", ofpFlowModFlagsRecord);
 
       byte[] p = {0,0};
       GenericData.Fixed pad = new GenericData.Fixed(ofpFlowModBaseSchema, p);
-      ofpFlowModRecord.put("pad", pad);
+      ofpFlowModBaseRecord.put("pad", pad);
 
       ofpMatchRecord.put("type", new EnumSymbol(ofpMatchTypeSchema, "OFPMT_OXM"));
         
-        ByteBuffer matchLenBuffer = ByteBuffer.allocate(2);
-        matchLenBuffer.put((byte)4);
-        matchLenBuffer.put((byte)0);
-        ofpMatchRecord.put("length", matchLenBuffer);
+      ByteBuffer matchLenBuffer = ByteBuffer.allocate(2);
+      matchLenBuffer.put((byte)4);
+      matchLenBuffer.put((byte)0);
+      ofpMatchRecord.put("length", matchLenBuffer);
         
-        ByteBuffer oxmBuffer = ByteBuffer.allocate(4);
-        oxmBuffer.put((byte)0);
-        oxmBuffer.put((byte)0);
-        oxmBuffer.put((byte)0);
-        oxmBuffer.put((byte)0);
-        ofpMatchRecord.put("oxm_fields", oxmBuffer);
+      ByteBuffer oxmBuffer = ByteBuffer.allocate(4);
+      oxmBuffer.put((byte)0);
+      oxmBuffer.put((byte)0);
+      oxmBuffer.put((byte)0);
+      oxmBuffer.put((byte)0);
+      ofpMatchRecord.put("oxm_fields", oxmBuffer);
         
-        ofpFlowModRecord.put("match", ofpMatchRecord);
+      ofpFlowModBaseRecord.put("match", ofpMatchRecord);
 	    
-        DatumWriter<GenericRecord> writer = new GenericDatumWriter<GenericRecord>(ofpFlowModSchema);
-	    Encoder encoder = EncoderFactory.get().binaryNonEncoder(out, null);
+      DatumWriter<GenericRecord> writer = new GenericDatumWriter<GenericRecord>(ofpFlowModSchema);
+	   Encoder encoder = EncoderFactory.get().binaryNonEncoder(out, null);
 	    
 		try {
 			writer.write(ofpFlowModRecord, encoder);
