cbd9a79b7de020fbea6d4fbd27d6be539975319e
Dmitry Orekhov <Dorekhov@infoblox.com>
2013-10-23 18:30:21 +0300

    all binary dependencies are moved to deps

--- lib/orig/get-jars.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-wget http://jackson.codehaus.org/1.9.11/jackson-all-1.9.11.jar
-wget http://dl.bintray.com/netty/downloads/netty-3.7.0.Final-dist.tar.bz2
-wget http://downloads.typesafe.com/akka/akka-2.2.1.zip
-
-echo "manually download these files ..."
-echo "http://restlet.org/download/current#release=stable&edition=jse&distribution=zip"
