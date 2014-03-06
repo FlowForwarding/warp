5baf878c314bf384c140e157611deb936b505e94
dmitry-orekhov <dmitry.orekhov@gmail.com>
2014-03-06 23:35:29 +0300

    bugs fixing

--- src/main/java/org/flowforwarding/warp/protocol/ofitems/IOFItem.java
+++ src/main/java/org/flowforwarding/warp/protocol/ofitems/IOFItem.java
@@ -1,9 +1,12 @@
 package org.flowforwarding.warp.protocol.ofitems;
 
+import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericContainer;
 
 public interface IOFItem {
    
    public GenericContainer get();
-
+   
+   public String getName();
+   public Schema getSchema();
 }
