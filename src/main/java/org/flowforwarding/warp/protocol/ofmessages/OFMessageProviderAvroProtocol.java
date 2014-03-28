dfae9e0aa35161ebd28b84246d82a27534da0b37
dmitry-orekhov <dmitry.orekhov@gmail.com>
2014-03-28 19:28:23 +0300

    Initial commit

---
+++ src/main/java/org/flowforwarding/warp/protocol/ofmessages/OFMessageProviderAvroProtocol.java
@@ -0,0 +1,22 @@
+/**
+ * © 2013 FlowForwarding.Org
+ * All Rights Reserved.  Use is subject to license terms.
+ */
+package org.flowforwarding.warp.protocol.ofmessages;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.flowforwarding.warp.protocol.ofitems.IOFItemBuilder;
+
+/**
+ * @author Infoblox Inc.
+ *
+ */
+public class OFMessageProviderAvroProtocol {
+   
+   protected Map<String, IOFItemBuilder> builders = new HashMap<>();
+   
+   
+
+}
