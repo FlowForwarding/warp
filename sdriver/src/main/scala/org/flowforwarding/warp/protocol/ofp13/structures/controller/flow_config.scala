b2977643777c8251bf68a7ce867dcaba7c4d09dd
Dmitry Orekhov <dmitry_orekhov@epam.com>
2014-09-10 19:50:03 +0300

    sdriver recommit

---
+++ sdriver/src/main/scala/org/flowforwarding/warp/protocol/ofp13/structures/controller/flow_config.scala
@@ -0,0 +1,46 @@
+/*
+ * © 2013 FlowForwarding.Org
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * @author Vitaliy Savkin (Vitaliy_Savkin@epam.com)
+ */
+package org.flowforwarding.warp.protocol.ofp13.structures
+
+import com.gensler.scalavro.types.supply._
+
+import org.flowforwarding.warp.protocol.ofp13.structures.ofp_table.OFP_TABLE
+import org.flowforwarding.warp.protocol.ofp13.structures.ofp_table_config.OFP_TABLE_CONFIG
+
+import org.flowforwarding.warp.protocol.dynamic.DynamicPath
+import ofp_length._
+
+/* Table numbering. Tables can use any number up to OFPT_MAX. */
+object ofp_table extends ByteEnum with AllowUnspecifiedValues[Byte]{
+  type OFP_TABLE = Value
+  /* Last usable table number. */
+  val OFPTT_MAX = ##(0xfe)
+  /* Fake tables. */
+  val OFPTT_ALL = ##(0xff) /* Wildcard table used for table config, flow stats and flow deletes. */
+}
+
+/* Configure/Modify behavior of a flow table */
+case class ofp_table_mod private[protocol] (
+  header: ofp_header,
+  table_id: OFP_TABLE,      /* ID of the table, OFPTT_ALL indicates all tables */
+  pad: Pad3,                /* Pad to 32 bits */
+  config: OFP_TABLE_CONFIG) /* Bitmap of OFPTC_* flags */
+
+object ofp_table_mod{
+
+  private[protocol] def build(@DynamicPath("header", "xid") xid: UInt32, table_id: OFP_TABLE, config: OFP_TABLE_CONFIG) =
+    ofp_table_mod(ofp_header(OFPL_TABLE_MOD_LEN, xid), table_id, Pad3(), config)
+
+  def apply(xid: Int, table_id: OFP_TABLE, config: OFP_TABLE_CONFIG): ofp_table_mod = build(UInt32.fromInt(xid), table_id, config)
+}
+
+/* Flags to configure the table. Reserved for future use. */
+object ofp_table_config extends DWordEnum { // DWordBitmap?
+  type OFP_TABLE_CONFIG = Value
+  val OFPTC_DEPRECATED_MASK = ##(3) /* Deprecated bits */
+}
+
---
+++ sdriver/src/main/scala/org/flowforwarding/warp/protocol/ofp13/structures/symmetric/hello.scala
@@ -0,0 +1,68 @@
+/*
+ * © 2013 FlowForwarding.Org
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * @author Vitaliy Savkin (Vitaliy_Savkin@epam.com)
+ */
+package org.flowforwarding.warp.protocol.ofp13.structures
+
+import org.flowforwarding.warp.protocol.ofp13.structures.ofp_hello_elem_type.OFP_HELLO_ELEM_TYPE
+
+import com.gensler.scalavro.types.supply._
+import org.flowforwarding.warp.protocol.ofp13.structures.ofp_hello.ofp_hello_elem
+import org.flowforwarding.warp.protocol.dynamic.DynamicPath
+import ofp_length._
+
+/* OFPT_HELLO. This message includes zero or more hello elements having
+ * variable size. Unknown elements types must be ignored/skipped, to allow
+ * for future extensions. */
+case class ofp_hello private[protocol] (header: ofp_header, elements: RawSeq[ofp_hello_elem])/* List of hello elements - 0 or more */ // TODO: skip unrecognized headers
+
+object ofp_hello extends RawSeqFieldsInfo{
+  val rawFieldsLengthCalculator: LengthCalculator = { case 1 => bodyLength }
+
+  trait ofp_hello_elem extends WordTaggedUnion[singletonUnion[ofp_hello_elem_versionbitmap], OFP_HELLO_ELEM_TYPE]{
+    def length: UInt16
+    def intLength = UInt16.toShort(length)
+  }
+
+  private[protocol] def build(@DynamicPath("header", "xid") xid: UInt32, elements: RawSeq[ofp_hello_elem] = RawSeq()) = {
+    val header = ofp_header(OFPL_HELLO_LEN + elements.map { _.intLength }.sum, xid)
+    ofp_hello(header, elements)
+  }
+
+  def apply(xid: Int, elements: ofp_hello_elem*): ofp_hello = build(UInt32.fromInt(xid), elements)
+}
+
+/* Hello elements types. */
+object ofp_hello_elem_type extends WordEnum{
+  type OFP_HELLO_ELEM_TYPE = Value
+  val OFPHET_VERSIONBITMAP = ##(1) /* Bitmap of version supported. */
+}
+
+/* Common header for all Hello Elements */
+case class ofp_hello_elem_header(hType: OFP_HELLO_ELEM_TYPE, /* One of OFPHET_*. */
+                                 length: UInt16)             /* Length in bytes of the element, including this header, excluding padding. */
+
+/* Version bitmap Hello Element */
+case class ofp_hello_elem_versionbitmap private[protocol] (//hType: OFP_HELLO_ELEM_TYPE = OFPHET_VERSIONBITMAP, /* One of OFPHET_*. */
+                                        length: UInt16,             /* Length in bytes of the element, including this header, excluding padding. */
+                                        /* Followed by:
+                                         * - Exactly (length - 4) bytes containing the bitmaps, then
+                                         * - Exactly (length + 7)/8*8 - (length) (between 0 and 7)
+                                         * bytes of all-zero bytes */
+                                        bitmaps: RawSeq[UInt32],
+                                        pad: RawSeq[Pad1]) extends ofp_hello.ofp_hello_elem/* List of bitmaps - supported versions */
+
+object ofp_hello_elem_versionbitmap extends RawSeqFieldsInfo{
+  val rawFieldsLengthCalculator: LengthCalculator = {
+    case 1 => s => lenField(0)(s) - 4
+    case 2 => s =>  /* (lenField(1)(s) + 7) / 8 * 8 */ 0   // TODO: fix calculation
+  }
+
+  private[protocol] def build(bitmaps: RawSeq[UInt32] = RawSeq()) = {
+    ofp_hello_elem_versionbitmap(UInt16.fromShort((4 + bitmaps.length * 4).toShort), bitmaps, RawSeq() /* fix length of padding*/)
+  }
+
+  def apply(bitmaps: Int*): ofp_hello_elem_versionbitmap = build(RawSeq(bitmaps map UInt32.fromInt: _*))
+}
