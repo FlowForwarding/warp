297b43af48815bce5fb8a485a76560c9a6bc47aa
Dmitry Orekhov <Dmitry_Orekhov@epam.com>
2014-11-18 13:21:52 +0300

    pom.xml: avro 1,8

---
+++ driver/src/main/java/org/flowforwarding/warp/App.java
@@ -0,0 +1,13 @@
+package org.flowforwarding.warp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
