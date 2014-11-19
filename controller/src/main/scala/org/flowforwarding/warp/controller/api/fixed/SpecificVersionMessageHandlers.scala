8ec81de298af160bbd2d52d3e345903c18c2683e
Vitaliy Savkin <vitaliy_savkin@epam.com>
2014-11-19 17:30:50 +0200

    Controller: added cache for message handling methods.

--- controller/src/main/scala/org/flowforwarding/warp/controller/api/fixed/SpecificVersionMessageHandlers.scala
+++ controller/src/main/scala/org/flowforwarding/warp/controller/api/fixed/SpecificVersionMessageHandlers.scala
@@ -24,6 +24,14 @@ abstract class SpecificVersionMessageHandlers[Self <: SpecificVersionMessageHand
                                               D <: MessagesDescriptionHelper[Self]: ClassTag](controllerBus: ControllerBus, versionCode: UByte)
   extends MessageHandlers[DynamicStructure, DynamicStructureBuilder[_]](controllerBus){
 
+  private def castDescription(driver: MessageDriver[_]): Try[D] = Try { implicitly[ClassTag[D]].runtimeClass.asInstanceOf[Class[D]].cast(driver) }
+
+  override def compatibleWith(factory: MessageDriverFactory[_]): Boolean = {
+    super.compatibleWith(factory) && castDescription(factory.get(versionCode)).isSuccess
+  }
+
+  private val handlersCache = scala.collection.mutable.Map[Class[_], Option[Method]]()
+
   private def canHandleClass(c: Class[_])(m: Method)  = {
     val L = classOf[Long]
     m.getParameterTypes match {
@@ -32,20 +40,22 @@ abstract class SpecificVersionMessageHandlers[Self <: SpecificVersionMessageHand
     }
   }
 
-  private def castDescription(driver: MessageDriver[_]): Try[D] = Try { implicitly[ClassTag[D]].runtimeClass.asInstanceOf[Class[D]].cast(driver) }
-
-  override def compatibleWith(factory: MessageDriverFactory[_]): Boolean = {
-    super.compatibleWith(factory) && castDescription(factory.get(versionCode)).isSuccess
-  }
-
-  private def handle(dpid: ULong, message: FixedOfpMessage): Try[Array[FixedMessageInput]] =
-    this.getClass.getMethods find canHandleClass(message.getClass) match {
+  private def handle(dpid: ULong, message: FixedOfpMessage): Try[Array[FixedMessageInput]] = {
+    val msgClass = message.getClass
+    if(!handlersCache.contains(msgClass)) {
+      val lookupRes = this.getClass.getMethods find canHandleClass(msgClass)
+      lookupRes foreach { _.setAccessible(true) }
+      handlersCache(msgClass) = lookupRes
+    }
+    handlersCache(msgClass) match {
       case Some(m) => Try {
-        m.setAccessible(true)
         m.invoke(this, Long.box(dpid.signed), message).asInstanceOf[Array[FixedMessageInput]]
       }
-      case None => Failure(new RuntimeException(s"Unable to handle message of type ${message.getClass}: no appropriate method found."))
+      case None => Failure {
+        new RuntimeException(s"Unable to handle message of type $msgClass: no appropriate method found.")
+      }
     }
+  }
 
   def testIncomingMessage(predicate: IncomingMessagePredicate): PartialFunction[MessageEnvelope, Boolean] = {
     case SwitchIncomingMessage(id, driver: MessagesDescriptionHelper[_], msg: DynamicStructure) if driver.versionCode == this.versionCode =>
