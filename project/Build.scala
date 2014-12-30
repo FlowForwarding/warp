d3dbc8918b39fc15759646e685ae2cd661957806
Vitaliy Savkin <vitaliy_savkin@epam.com>
2014-12-30 17:16:10 +0200

    Build: added unidoc plugin.

--- project/Build.scala
+++ project/Build.scala
@@ -2,13 +2,14 @@ import sbt._
 import Keys._
 
 import sbtassembly.AssemblyPlugin.autoImport._
+import sbtunidoc.Plugin._
+import UnidocKeys._
 
 import com.typesafe.sbt.SbtGhPages.ghpages
 import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.SbtGit.git
 
 object Build extends Build{
-  //import sbtassembly.Plugin._
   import Dependencies._
 
   val SCALA_MAJOR_VERSION = "2.11"
@@ -20,36 +21,53 @@ object Build extends Build{
 
   val loggingLibs = Seq(scala_logging, logback_core, logback)
 
+  val githubUser = "Vitaliy-Savkin"
 
-  def warpCommonSettings(projectName: String) =
+  val warpCommonSettings =
     Defaults.coreDefaultSettings ++
-    ghpages.settings ++
-    site.settings ++
-    site.includeScaladoc("latest/api/" + projectName) ++
     Seq(
       organization  := "org.flowforwarding",
       version       := "0.5",
       scalaVersion  := SCALA_VERSION,
       scalacOptions := Seq("-deprecation", "-unchecked", "-encoding", "utf8"),
-      git.remoteRepo := "git@github.com:Vitaliy-Savkin/warp.git",
       resolvers     ++= Dependencies.resolutionRepos,
+      scalacOptions in (Compile, doc) ++= Opts.doc.sourceUrl(s"https://github.com/$githubUser/warp/blob/master€{FILE_PATH}.scala") ++
+                                          Seq("-sourcepath",  new java.io.File(".").getCanonicalPath),
       libraryDependencies ++= loggingLibs
     )
 
-  lazy val driver_api =  Project("driver-api", file("./driver-api"), settings = warpCommonSettings("driver-api"))
+  val warp = Project("warp", file("."), settings = warpCommonSettings ++
+                                                   unidocSettings ++
+                                                   ghpages.settings ++
+                                                   site.settings ++
+                                                   site.includeScaladoc())
+    .settings(
+      unidocProjectFilter in (ScalaUnidoc, unidoc) := inAnyProject -- inProjects(core, util, of_driver),
+      site.addMappingsToSiteDir(mappings in (ScalaUnidoc, packageDoc), "latest/api"),
+      git.remoteRepo := s"git@github.com:$githubUser/warp.git"
+    )
+    .aggregate(driver_api,
+               controller,
+               driver_api_ofp13, driver_api_ofp13_adapter,
+               sdriver, sdriver_ofp13, sdriver_ofp13_adapter,
+               of_driver,
+               core, util,
+               demo)
+
+  lazy val driver_api = Project("driver-api", file("./driver-api"), settings = warpCommonSettings)
     .settings(
       libraryDependencies ++= compile(spire, config) ++ test(scalatest)
     )
 
 
-  lazy val driver_api_ofp13 = Project("driver-api-ofp13", file("./driver-api-ofp13"), settings = warpCommonSettings("driver-api-ofp13"))
+  lazy val driver_api_ofp13 = Project("driver-api-ofp13", file("./driver-api-ofp13"), settings = warpCommonSettings)
     .dependsOn(driver_api)
     .settings(
       libraryDependencies ++= compile(spire, config) ++ test(scalatest)
     )
 
 
-  lazy val driver_api_ofp13_adapter = Project("driver-api-ofp13-adapter", file("./driver-api-ofp13-adapter"), settings = warpCommonSettings("driver-api-ofp13-adapter"))
+  lazy val driver_api_ofp13_adapter = Project("driver-api-ofp13-adapter", file("./driver-api-ofp13-adapter"), settings = warpCommonSettings)
     .dependsOn(controller, driver_api, driver_api_ofp13)
     .settings(
       libraryDependencies ++= compile(spire, config) ++ test(scalatest)
@@ -58,13 +76,13 @@ object Build extends Build{
 
   lazy val of_driver_assemblySettings = Seq(mainClass := Some("org.flowforwarding.warp.jcontroller.JController"))
 
-  lazy val of_driver = Project("of_driver", file("./of_driver"), settings = warpCommonSettings("of_driver") ++ of_driver_assemblySettings)
+  lazy val of_driver = Project("of_driver", file("./of_driver"), settings = warpCommonSettings ++ of_driver_assemblySettings)
     .dependsOn(driver_api)
     .settings(
       libraryDependencies ++= compile(akka, netty, jackson_core_asl, jackson_mapper_asl)
     )
 
-  lazy val controller = Project("controller", file("./controller"), settings = warpCommonSettings("controller"))
+  lazy val controller = Project("controller", file("./controller"), settings = warpCommonSettings)
     .dependsOn(driver_api)
     .settings(
       libraryDependencies ++= compile(akka, akka_slf4j, spray_json, spray_http, spray_can, spray_routing, spire) ++ test(scalatest)
@@ -75,24 +93,24 @@ object Build extends Build{
 //      unmanagedBase <<= baseDirectory { base => base / "lib" }
 //    ).dependsOn(controller)
 
-  lazy val sdriver = Project("sdriver", file("./sdriver"), settings = warpCommonSettings("sdriver"))
+  lazy val sdriver = Project("sdriver", file("./sdriver"), settings = warpCommonSettings)
     .settings(
       libraryDependencies ++= compile(scala_reflect, scala_compiler, spire)
     ).dependsOn(util, core, driver_api)
 
 
-  lazy val sdriver_ofp13 = Project("sdriver-ofp13", file("./sdriver-ofp13"), settings = warpCommonSettings("sdriver-ofp13"))
+  lazy val sdriver_ofp13 = Project("sdriver-ofp13", file("./sdriver-ofp13"), settings = warpCommonSettings)
     .settings(
       libraryDependencies ++= compile(scala_reflect, scala_compiler)
     ).dependsOn(util, core, sdriver, driver_api_ofp13)
 
 
-  lazy val sdriver_ofp13_adapter = Project("sdriver-ofp13-adapter", file("./sdriver-ofp13-adapter"), settings = warpCommonSettings("sdriver-ofp13-adapter"))
+  lazy val sdriver_ofp13_adapter = Project("sdriver-ofp13-adapter", file("./sdriver-ofp13-adapter"), settings = warpCommonSettings)
     .settings(
       libraryDependencies ++= compile(scala_reflect, scala_compiler)
     ).dependsOn(controller, sdriver, sdriver_ofp13)
 
-  lazy val demo = Project("demo", file("./demo"), settings = warpCommonSettings("demo"))
+  lazy val demo = Project("demo", file("./demo"), settings = warpCommonSettings)
     .settings(
       libraryDependencies ++= compile(spray_client, spray_json, spray_httpx)
     )
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,4 +4,6 @@ resolvers += "jgit-repo" at "http://download.eclipse.org/jgit/maven"
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-ghpages" % "0.5.3")
 
+addSbtPlugin("com.eed3si9n" % "sbt-unidoc" % "0.3.1")
+
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.12.0")
